ef48da9d91c18089daecde3b7a29635a6175276f
Vladyslav Romanchuk <senioroman4uk@gmail.com>
2023-12-12 20:00:17 -0800

    solved day 4 part 2

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -4,67 +4,88 @@ using Helpers;
 
 var fileName = Helper.GetFileName(args);
 
-List<int> results = new();
-
-
-foreach (var card in ParseCards(fileName))
-{
-    var winningNumbersCount = card.TotalCount - card.LooseNumbers.Count;
-    int winningPoints = winningNumbersCount > 0 ? Pow(2, winningNumbersCount - 1) : 0;
-    results.Add(winningPoints);
-}
-
-int result = results.Sum();
-
-Console.WriteLine("winning points per card:");
-foreach (int points in results)
-{
-    Console.WriteLine(points);
-}
-Console.WriteLine("Total points:");
-Console.WriteLine(result);
-
-int Pow(int @base, int power)
-{
-    if (power < 0)
-    {
-        throw new Exception("power must be greater than 0");
-    }
-    
-    if (power == 0)
-    {
-        return 1;
-    }
-
-    if (power == 1)
-    {
-        return @base;
-    }
-
-    var result = @base;
-    for (int i = 1; i < power; i++)
-    {
-        result *= @base;
-    }
-
-    return result;
-}
+var cards = ParseCards(fileName).ToList();
+Part1(cards);
+Part2(cards);
 
 IEnumerable<Card> ParseCards(string fileName)
 {
-    int id = 0;
     foreach (string line in File.ReadLines(fileName))
     {
         var numbers = line.Split(':', StringSplitOptions.TrimEntries).Last().Split('|', StringSplitOptions.TrimEntries);
         var winningNumbers = numbers[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
         var cardNumbers = numbers[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
-        yield return new Card(id++, winningNumbers, cardNumbers);
+        yield return new Card(winningNumbers, cardNumbers);
     }
 }
 
-internal record Card(int Id, HashSet<int> WinningNumbers, HashSet<int> CardNumbers)
+void Part1(List<Card> list)
 {
-    public int TotalCount { get; } = CardNumbers.Count;
-    
-    public HashSet<int> LooseNumbers { get; } = CardNumbers.Except(WinningNumbers).ToHashSet();
+    List<int> part1Results = new();
+    int Pow(int @base, int power)
+    {
+        if (power < 0)
+        {
+            throw new Exception("power must be greater than 0");
+        }
+
+        if (power == 0)
+        {
+            return 1;
+        }
+
+        if (power == 1)
+        {
+            return @base;
+        }
+
+        var result = @base;
+        for (int i = 1; i < power; i++)
+        {
+            result *= @base;
+        }
+
+        return result;
+    }
+
+    foreach (var card in list)
+    {
+        var winningNumbersCount = card.PresentWinningNumbers.Count;
+        int winningPoints = winningNumbersCount > 0 ? Pow(2, winningNumbersCount - 1) : 0;
+        part1Results.Add(winningPoints);
+    }
+
+    int part1Result = part1Results.Sum();
+
+    Console.WriteLine("winning points per card:");
+    foreach (int points in part1Results)
+    {
+        Console.WriteLine(points);
+    }
+
+    Console.WriteLine("Part1 result - Total Points:");
+    Console.WriteLine(part1Result);
+}
+
+void Part2(List<Card> cards)
+{
+    var cardCounters = cards.Select<Card, (Card Card, int Count)>(c => (c, 1)).ToArray();
+    for (var i = 0; i < cardCounters.Length; i++)
+    {
+        var counter = cardCounters[i];
+        var winningNumbersCount = counter.Card.PresentWinningNumbers.Count - 1;
+        for (int j = i + 1; j < cardCounters.Length && winningNumbersCount >= 0; j++, winningNumbersCount--)
+        {
+            cardCounters[j].Count += counter.Count;
+        }
+    }
+
+    var result = cardCounters.Sum(c => c.Count);
+    Console.WriteLine("Part2 result - Total Cards:");
+    Console.WriteLine(result);
+}
+
+internal record Card(HashSet<int> WinningNumbers, HashSet<int> CardNumbers)
+{
+    public HashSet<int> PresentWinningNumbers { get; } = CardNumbers.Intersect(WinningNumbers).ToHashSet();
 }
